31d5a1d178e56f774f66787a54b3eeb091a82abf
Kevin Tran <kevin_tran95@yahoo.com>
2021-10-19 21:49:00 -0700

    highscore should work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
 var scoreCount = 0;
 var timer = 75;
 var timerInterval;
+var startButton = document.querySelector ("#startButton");
+var headerOne = document.querySelector ("h1");
+var userBox = document.querySelector (".userBox");
+var description = document.querySelector(".description");
+var answerBox = document.querySelector(".answerBox");
+var count = document.querySelector(".count");
+var timerHTML = document.querySelector(".timer");
+var allScores = [];
 
+
+// Countdown
 function timerCountdown() {
   timerInterval = setInterval(function() {
     if (timer > 0){
@@ -17,5 +27,22 @@ function timerCountdown() {
       clearInterval(timerInterval);
     }
   }, 1000);
+}
+
+// Show highscore
+function showHighscore () {
+  var highScore = JSON.parse(localStorage.getItem('userInfo'));
+  console.log(highScore);
+  allScores = highScore;
+
+  for (var i = 0; i < allScores.length; i++) {
+    var singleScore = allScores[i];
+
+    var li = document.createElement('li');
+    li.textContent = 'Initials: ' + singleScore.initials + "Time: " +singleScore.score;
+    
+    ol.appendChild(li);
+  }
+
 }
 
